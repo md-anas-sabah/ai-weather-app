6e68ca047d899464fb9e60f307201a39e9f0dac6
Md Anas Sabah <mdanassabah@gmail.com>
2023-05-20 00:15:43 +0530

    update: Integrated ApolloClient to connect with backend

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -45,7 +45,7 @@ const handleSelectedCountry = (option :option) => {
 } 
 const handleSelectedCity = (option: cityOption) => {
     setSelectedCity(option)
-    // router.push(`/location/${option?.value.latitude}/${option?.value.longitude}`)
+    router.push(`/location/${option?.value.name}/${option?.value.latitude}/${option?.value.longitude}`)
 }
   return (
     <div className='space-y-4'>
